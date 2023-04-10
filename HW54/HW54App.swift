c8fd886aede623dc865214a5b77ce900db9decd4
Datkayim <dkeldibekova08@gmail.com>
2023-04-10 10:19:10 +0600

    Initial Commit

---
+++ HW54/HW54App.swift
@@ -0,0 +1,17 @@
+//
+//  HW54App.swift
+//  HW54
+//
+//  Created by Jarae on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct HW54App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
